cdeb91fe907e84a8af6f022a363136acba3bb483
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2020-03-17 20:52:57 +0100

    updated init

--- normalize_text_to_df/__init__.py
+++ normalize_text_to_df/__init__.py
@@ -1,1 +1,2 @@
-from normalize_text_to_df.text_to_df import *
+from normalize_text_to_df.text_to_df import text_to_df, TextToDf
+from normalize_text_to_df.methods.stanfordnlp_to_df import stanfordnlp_to_df

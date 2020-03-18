ac7c0c9bfed1f308f8ad8cb3b1d91962ac2f06b4
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2020-03-18 22:01:20 +0100

    minor

--- text_to_x/__init__.py
+++ text_to_x/__init__.py
@@ -1,2 +1,2 @@
 from text_to_x.text_to_df import TextToDf, texts_to_dfs
-from text_to_sentiment import TextToSentiment, texts_to_dfs
+from text_to_x.text_to_sentiment import TextToSentiment, texts_to_dfs

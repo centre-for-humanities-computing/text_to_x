a8527633cf20e086598100b23ef6a30c693c2293
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2020-03-26 16:03:06 +0100

    SvdEmbeddings class for training and basic operations
    * svd embeddings class for training and basic operations
    
    * flake8 styling
    
    * added doctest, loops to list comprehension
    
    * forgot newline at the end of file
    
    * fix duplicate
    
    * deleted showcase notebook in the root dir
    
    Co-authored-by: Ludvig Olsen & Kenneth Enevoldsen

--- text_to_x/__init__.py
+++ text_to_x/__init__.py
@@ -1,4 +1,5 @@
 from text_to_x.text_to_tokens import TextToTokens
 from text_to_x.text_to_sentiment import TextToSentiment
+from text_to_x.text_to_embeddings import SvdEmbeddings
 from text_to_x.texts import Texts
 from text_to_x.utils import get_test_data

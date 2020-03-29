91f3a1d363c0d75c199f22fa45a2e5bae3054014
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2020-03-29 19:16:04 +0200

    updating filenaming

--- text_to_x/__init__.py
+++ text_to_x/__init__.py
@@ -1,4 +1,4 @@
-from text_to_x.text_to_tokens import TextToTokens
-from text_to_x.text_to_sentiment import TextToSentiment
-from text_to_x.texts_to import TextsTo
+from text_to_x.TextToTokens import TextToTokens
+from text_to_x.TextToSentiment import TextToSentiment
+from text_to_x.TextTo import TextTo
 from text_to_x.utils import get_test_data
--- text_to_x/text_to_topic.py
+++
@@ -1,7 +0,0 @@
-"""
-"""
-from text_to_x.text_to_x import TextToX
-
-
-class TextToTokens(TextToX):
-    pass

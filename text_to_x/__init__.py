479397f85977b4e2aa33ca57b7cd825a8d02862d
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2020-03-30 22:35:17 +0200

    update topic modelling

--- text_to_x/__init__.py
+++ text_to_x/__init__.py
@@ -1,4 +1,6 @@
 from text_to_x.TextToTokens import TextToTokens
+from text_to_x.TextToTopic import TextToTopic
+from text_to_x.TextToTopic import TextToTokensWrapper
 from text_to_x.TextToSentiment import TextToSentiment
 from text_to_x.TextTo import TextTo
 from text_to_x.text_to_embeddings import SvdEmbeddings

28cd49335d77428e114ebd5da3e0d55b67e56b65
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2020-04-01 21:17:00 +0200

    added twitter format

--- text_to_x/TextToTokens.py
+++ text_to_x/TextToTokens.py
@@ -1,6 +1,9 @@
 """
 """
+import re
 from warnings import warn
+
+import numpy as np
 
 from nltk.stem.snowball import SnowballStemmer
 from nltk.stem.snowball import PorterStemmer
@@ -229,6 +232,63 @@ class TextToTokens(TextToX):
             return df
         dfs = [casings_single_df(df) for df in dfs]
         return dfs
+
+    def twitter_format(self):
+        """
+        call this function to deal with emoticons, hashtags (#) and at (@)
+        it will add to 'ner' TWITTER_USER and HASHTAG
+        and will change POS tag for emojis to SYM_EMOJI
+        """
+
+        # regex replies (@something)
+        # twitter username can be letters or numbers
+        at_pattern = r'\B@\w*[a-zA-Z0-9]+\w*'
+
+        # regex hashtags (#something)
+        # twitter hashtag can't start with a number
+        hash_pattern = r'\B#\w*[a-zA-Z]+\w*'
+
+        # regex emojis
+        # unicode characters in certain range
+        emoji_pattern = re.compile("["
+                                u"\U0001F600-\U0001F64F"  # emoticons
+                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                u"\U00002702-\U000027B0"
+                                u"\U000024C2-\U0001F251"
+                                "]+", flags=re.UNICODE)
+
+        # regex url
+        # this is a monster, but works wonders
+        url_pattern = re.compile(r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})')
+
+        for i, df in enumerate(self.__dfs):
+            hashtag_idx = np.array(df['token'] == "#")
+
+            idx_offset = np.insert(hashtag_idx, 0, False)[:-1]
+            # add hashtag to token
+            df['token'][idx_offset] = '#' + df['token'][idx_offset]
+            df['lemma'][idx_offset] = '#' + df['lemma'][idx_offset]
+            # remove hashtag columns
+            df = df.loc[np.invert(hashtag_idx)]
+            hashtag_idx = np.array(df['token'].str.match(hash_pattern))
+            df['ner'][hashtag_idx] = "HASHTAG"
+
+            at_idx = np.array(df['token'] == "@")
+            idx_offset = np.insert(at_idx, 0, False)[:-1]
+            df['token'][idx_offset] = '@' + df['token'][idx_offset]
+            df['lemma'][idx_offset] = '@' + df['lemma'][idx_offset]
+            df = df.loc[np.invert(at_idx)]
+            at_idx = np.array(df['token'].str.match(at_pattern))
+            df['ner'][at_idx] = "TWITTER_USER"
+
+            emoji_idx = np.array(df['token'].str.match(emoji_pattern))
+            df['upos'][emoji_idx] = "SYM_EMOJI"
+
+            url_idx = np.array(df['token'].str.match(url_pattern))
+            df['ner'][url_idx] = "URL"
+            self.__dfs[i] = df
 
 
 if __name__ == "__main__":
